82cdb4192e2fa83b45ceb8e871eff14d45409a75
JM <michael.renon@kojiesan.com>
2026-03-17 17:25:04 +0800

    optimize inventory ending sell in data

--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'sysprodata' => [
+        'url'   => env('API_URL_SYSPRODATA'),
+        'token' => env('API_TOKEN_SYSPRODATA'),
+    ],
+
 ];
